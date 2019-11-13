75fe9e7175b4596228f3e5e44dbd61484ad52745
Antoine Lequeux <lequeuxa@gmail.com>
2019-11-13 17:29:45 +0100

    able to create a new weekly challenge from list of challenges

--- app/controllers/weekly_challenges_controller.rb
+++ app/controllers/weekly_challenges_controller.rb
@@ -4,6 +4,14 @@ class WeeklyChallengesController < ApplicationController
   end
 
   def create
+    @weekly_challenge = WeeklyChallenge.new
+    challenge = Challenge.find(params[:format])
+    @weekly_challenge.challenge = challenge
+    # @weekly_challenge.week = ?
+    # @weekly_challenge.year = ?
+    @weekly_challenge.user = current_user
+    @weekly_challenge.save
+    redirect_to challenge_path
   end
 
   def update
@@ -19,6 +27,10 @@ class WeeklyChallengesController < ApplicationController
   end
 
   private
+
+  def weekly_challenges_params
+    params.require(:weekly_challenge).permit(:status_challenge, :week, :year)
+  end
 
   def level_up?
     if @user.level.zero?
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   resources :challenges, only: [:show, :update]
   get '/dashboard/', to: 'pages#dashboard'
-  resources :weekly_challenges, only: [:update]
+  resources :weekly_challenges, only: [:create, :update]
   devise_for :users, controllers: { sessions: 'users/sessions' } do
     resources :sucesses, only: [:index, :new, :create, :update]
-    resources :weekly_challenges, only: [:index]
   end
   resources :categories, only: [:index] do
     resources :challenges, only: [:index]
