f955e1eb183b9739647a35cf58cdbb765c71118b
Antoine Lequeux <lequeuxa@gmail.com>
2019-11-15 16:01:12 +0100

    added root to display challenges done

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -6,10 +6,17 @@ class ChallengesController < ApplicationController
     @small_challenges = @challenges.where(size: false)
     @user_challenges = WeeklyChallenge.where(user: @user)
     @small_challenges_displayed = @small_challenges.where.not(weekly_challenges: @user_challenges)
-    raise
   end
 
   def show
     @challenge = Challenge.find(params[:id])
+  end
+
+  def index_done
+    @user = current_user
+
+    @user_wchallenges_done = @user.weekly_challenges.where(status_challenge: true)
+    # includes, ask Antoine or raise ticket later but it should display the challenges
+    # done meaning all challenges, not only the weekly ones.
   end
 end
