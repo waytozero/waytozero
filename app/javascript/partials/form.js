3c47368db1ef9b3859e7fce9e7f12b1c58d9a65d
Louis Grommen <louis.gr@live.be>
2019-11-15 16:48:04 +0100

    Progress bar

--- app/javascript/partials/form.js
+++ app/javascript/partials/form.js
@@ -1,14 +1,12 @@
-
-
 const addClass = (choice) => {
-  choice.classList.add("active");
+  choice.classList.add("active-form");
   console.log('test');
 };
 
 const eventForm = () => {
 const choices = document.querySelectorAll(".question-choice");
 choices.forEach(choice => choice.addEventListener('click', (event) => {
-  event.currentTarget.classList.toggle("active")
+  event.currentTarget.classList.toggle("active-form")
   event.currentTarget.querySelector("input").checked = !event.currentTarget.querySelector("input").checked
  }));
 };
