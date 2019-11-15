dee3c12b9030a5550b7029482a25f9f8eef8b4a1
Antoine Braconnier <antoine.braconnier@hotmail.com>
2019-11-15 13:25:56 +0100

    now the index displays only the challenges that has not been done

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,8 +1,12 @@
 class ChallengesController < ApplicationController
   def index
     @category = Category.find(params[:category_id])
+    @user = current_user
     @challenges = @category.challenges
     @small_challenges = @challenges.where(size: false)
+    @user_challenges = WeeklyChallenge.where(user: @user)
+    @small_challenges_displayed = @small_challenges.where.not(weekly_challenges: @user_challenges)
+    raise
   end
 
   def show
