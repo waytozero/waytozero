b12282db3264024cb995d73a73f9026e8c9ab3d7
Antoine Braconnier <antoine.braconnier@hotmail.com>
2019-11-22 14:03:25 +0100

    js fixed

--- app/javascript/partials/count.js
+++ app/javascript/partials/count.js
@@ -1,9 +1,11 @@
 import { CountUp } from 'countup.js';
 
 const count = (selector) => {
-const counter = document.querySelector(selector);
+  const counter = document.querySelector(selector);
+if (counter) {
   const countUp = new CountUp(counter, Number(counter.innerText));
   countUp.start();
+};
 }
 
 export {count};
