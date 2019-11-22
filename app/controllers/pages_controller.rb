fa5954b0688c8b9c43fe2b4160648906c0ec1aee
Louis Grommen <louis.gr@live.be>
2019-11-22 13:43:39 +0100

    Tree Counter

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,6 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
-  def home
-  end
-
   def dashboard
     @user = current_user
     @weekly_challenges = @user.weekly_challenges
