141813d7fa6bfe73a90dacfe0500c5298ded5c79
Louis Grommen <louis.gr@live.be>
2019-12-18 13:01:09 +0100

    Responsiveness improvements2

--- app/javascript/partials/emailForm.js
+++ app/javascript/partials/emailForm.js
@@ -46,6 +46,6 @@ sf.submitForm = () => {
 
 sf.input.addEventListener('keypress', (e) => sf.handleInputKeypress(e));
 
-document.querySelectorAll('button.trigger.shown').forEach(item => {
-  item.addEventListener('click', e => sf.clickHandler(e));
+document.querySelectorAll('button.trigger.shown').forEach(button => {
+  button.addEventListener('click', e => sf.clickHandler(e));
 })
