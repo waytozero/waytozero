87a58ec79dc3b47318c67f665b8e57817193e5f5
Antoine <lequeuxa@gmail.com>
2019-12-23 12:09:13 +0100

    test

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,6 @@ Rails.application.routes.draw do
     resources :achievement_number, only: [:index]
   end
   root to: 'pages#home'
-
+  #
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
